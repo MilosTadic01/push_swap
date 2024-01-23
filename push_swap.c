9e047564be57fa95352474c40df077c512f0d96d
Milos Tadic <mitadic@c4c5c2.42berlin.de>
2024-01-23 19:33:36 +0100

    next on the agenda: LiLi content pointing to arr[] members

--- push_swap.c
+++ push_swap.c
@@ -6,40 +6,73 @@
 /*   By: mitadic <mitadic@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 15:29:19 by mitadic           #+#    #+#             */
-/*   Updated: 2024/01/22 18:04:10 by mitadic          ###   ########.fr       */
+/*   Updated: 2024/01/23 18:36:14 by mitadic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-t_list	*set_list(char **argv, int size)
+// ultimately how to check the next smallest
+i++;
+smallest = arr_ind[i];
+// that's it bby; that's why we make that presorted arr_ind
+
+int	*index_arr(int *arr_raw, int size)
+{
+	int	i;
+	int	smol;
+	int	*arr_ind;
+
+	i = -1;
+	arr_ind = (int *)malloc(sizeof(int) * size);
+	while (++i < size)
+	{
+		if (i > 0 && arr_raw) 
+		arr_ind[i] = arr_raw[i]
+}
+
+// Heed this: maybe we don't even need two arrays! If we just linked-list to the
+// unsorted values, because that's what we need, then the linked list is pointing
+// to value, right? Ok, we need to play around with linked list first, before
+// we decide how many int arrays and which function is returning what.
+//
+// So Homework: give members of int * to linked-list contents, then see what
+// happens when you change the values.
+
+int	*conv_n_index(char **argv, int size)
 {
 	int	i;
 	int	j;
-	int	*arr;
+	int	*arr_raw;
 
 	i = 0;
-	arr = (int *)malloc(size * sizeof(int));
+	j = 0;
+	arr_raw = (int *)malloc(size * sizeof(int));
+	if (!arr_raw)
+		return (NULL);
 	while (argv[1][i])
 	{
-		if (ft_iswhite(argv[1][i])) // this is just handled by atoi, throw it out?
+		if (ft_iswhite(argv[1][i]))
 			i++;
 		else
 		{
-			arr[j] = ft_atoi(argv[1][i]);
+			arr_raw[j] = ft_atoi(&argv[1][i]);
 			while (argv[1][i] && ft_isdigit(argv[1][i]))
 				i++;
+			j++;
 		}
 	}
+	return (index_arr(arr_raw, size));
 }
 
 
-t_list	*get_list_size(char **argv)
+void	work_list(char **argv)
 {
 	int	i;
 	int	size;
-	t_list	*lst;
+	char	*arr_ind;
+	// t_list	*lst;
 
 	i = 0;
 	size = 0;
@@ -54,8 +87,9 @@ t_list	*get_list_size(char **argv)
 			size++;
 		}
 	}
-	lst = set_list(argv, size);
-	return (lst);
+	arr_ind = conv_n_index(argv, size);
+	// lst = set_list(argv, size);
+	// return (lst);
 }
 
 int	input_valid(char **argv)
@@ -66,7 +100,7 @@ int	input_valid(char **argv)
 	i = -1;
 	while (argv[1][++i])
 	{
-		c = argv[1][++i];
+		c = argv[1][i];
 		if (!(ft_isdigit(c) || ft_iswhite(c)) || ((c == 43 || \
 				c == 45) && !(ft_isdigit(argv[1][i + 1]))))
 			return (0);
@@ -76,10 +110,12 @@ int	input_valid(char **argv)
 
 int	main(int argc, char **argv)
 {
-	if (argc != 2)
+	if (argc != 2 || !input_valid(argv))
+	{
+		ft_printf("Error\n");
 		return (1);
-	if (!input_valid(argv))
-		return (1);
+	}
+	work_list(argv);
 	printf("Should be 0\n");
 	return (0);
 }
